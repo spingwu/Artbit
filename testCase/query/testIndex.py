0c6ef27463e45554f8ef841a9dd002fd79ce6390
spingwu <nancy2262>
2018-01-15 11:27:26 +0800

    增加testIndex

--- testCase/query/testIndex.py
+++ testCase/query/testIndex.py
@@ -4,22 +4,18 @@ from common import common
 from common.Log import MyLog
 import readConfig as readConfig
 from common import configHttp as configHttp
+import logging
 
-data = common.get_xls_bypandas("user.xlsx", "sendVerifyCode")
+data = common.get_xls_bypandas("query.xlsx", "index")
 Readconfig = readConfig.ReadConfig()
 cf = configHttp.ConfigHttp()
 
-
 @paramunittest.parametrized(*data)
-class testLogin(unittest.TestCase):
-
-    def setParameters(self, case_name, path, method, type, phone, countryCode, code, msg):
+class test_Login(unittest.TestCase):
+    def setParameters(self, case_name, path, method, code, msg):
         self.case_name = str(case_name)
         self.path = str(path)
         self.method = str(method)
-        self.type = str(type)
-        self.phone = str(phone)
-        self.countryCode = str(countryCode)
         self.code = int(code)
         self.msg = str(msg)
 
@@ -27,27 +23,23 @@ class testLogin(unittest.TestCase):
         self.case_name
 
     def setUp(self):
-        self.log=MyLog.get_log()
-        self.logger=self.log.get_logger()
+        self.log = MyLog.get_log()
+        self.logger = self.log.get_logger()
 
     def test_Login(self):
         self.url = self.path
         cf.set_url(self.url)
-        data1 = {'type': self.type, 'phone': self.phone, 'countryCode': self.countryCode}
-        cf.set_data(data1)
-        self.response = cf.postWithData()
+        self.response = cf.getwithoutheaders()
+        #print(self.response.text)
         self.checkResult()
 
     def tearDown(self):
-        self.log.build_case_line(self.info)
-
+ #       self.log.build_case_line(self.info)
+        pass
     def checkResult(self):
         self.info = self.response.json()
-
+        logging.info(self.info)
         #common.show_return_msg(self.response)
         self.assertEqual(self.info['code'], self.code)
         self.assertEqual(self.info['msg'], self.msg)
 
-
-if __name__ == "__main__":
-    pass
